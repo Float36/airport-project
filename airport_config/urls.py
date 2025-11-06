ee4a0e2e5032ec2484484adbea7865b5b0c06aec
Yura <yurastashchyshyn@gmail.com>
2025-11-06 22:27:21 +0200

    Add a jwt athethication

--- airport_config/urls.py
+++ airport_config/urls.py
@@ -1,7 +1,12 @@
-
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,6 +19,11 @@ urlpatterns = [
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
     path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    # URL for getting token
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # URL for refreshing access token
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
 
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
